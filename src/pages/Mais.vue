de1f8e18f187a9fbc500ceb2aaba22b8b7c015e7
IrfanCuambe <irfanbaptistacuambe@gmail.com>
2023-10-31 22:17:16 +0200

    run

---
+++ src/pages/Mais.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Saiba mais</h5>
+    <p>Bem-vindo à nossa página de ajuda.</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelpPage",
+});
+</script>
